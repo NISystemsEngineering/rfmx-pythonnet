2d4e26ad00fa78673dbadaccec6f46947a2dda88
Sean Moore <sean.moore@ni.com>
2019-12-11 16:53:00 -0600

    update server to load custom built wrappers

--- rpyc/server/RFmxService.py
+++ rpyc/server/RFmxService.py
@@ -8,11 +8,12 @@ import clr
 import sys
 import os
 
-# Add search path for NI .NET assemblies to system path
-sys.path.append(os.environ["ProgramFiles(x86)"] + r'\National Instruments\MeasurementStudioVS2010\DotNET\Assemblies\Current')
+# Add search paths for .NET assemblies to system path
+sys.path.append(os.environ["ProgramFiles(x86)"] + r"\National Instruments\MeasurementStudioVS2010\DotNET\Assemblies\Current")
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + "\\..\\..\\bin"))
 
 # Add in all of the IVI .NET paths for finding additional assemblies
-iviDotNetPath = os.environ["ProgramFiles"] + r'\IVI Foundation\IVI\Microsoft.NET\Framework64'
+iviDotNetPath = os.environ["ProgramFiles"] + r"\IVI Foundation\IVI\Microsoft.NET\Framework64"
 for x in os.walk(iviDotNetPath):
     sys.path.append(x[0])
 
@@ -42,16 +43,19 @@ class RFmxService(rpyc.Service):
     exposed_NationalInstruments = ImportDotNetSubmodule("NationalInstruments.Common", "NationalInstruments")
     exposed_ModularInstruments = ImportDotNetSubmodule("NationalInstruments.ModularInstruments.Common", "NationalInstruments.ModularInstruments")
 
-    # Import generation modules
+    # Import RFSG generation modules
     exposed_NIRfsg = ImportDotNetSubmodule("NationalInstruments.ModularInstruments.NIRfsg.Fx40", "NationalInstruments.ModularInstruments.NIRfsg")
     exposed_NIRfsgPlayback = ImportDotNetSubmodule("NationalInstruments.ModularInstruments.NIRfsgPlayback.Fx40", "NationalInstruments.ModularInstruments.NIRfsgPlayback")
 
-    # Import measurement modules
+    # Import RFmx measurement modules
     exposed_InstrMX = ImportDotNetSubmodule("NationalInstruments.RFmx.InstrMX.Fx40", "NationalInstruments.RFmx.InstrMX")
     exposed_SpecAnMX = ImportDotNetSubmodule("NationalInstruments.RFmx.SpecAnMX.Fx40", "NationalInstruments.RFmx.SpecAnMX")
     exposed_LteMX = ImportDotNetSubmodule("NationalInstruments.RFmx.LteMX.Fx40", "NationalInstruments.RFmx.LteMX")
     exposed_NRMX = ImportDotNetSubmodule("NationalInstruments.RFmx.NRMX.Fx40", "NationalInstruments.RFmx.NRMX")
     exposed_WlanMX = ImportDotNetSubmodule("NationalInstruments.RFmx.WlanMX.Fx40", "NationalInstruments.RFmx.WlanMX")
+
+    # Import measurement toolkit modules
+    exposed_WlanTK = ImportDotNetSubmodule("NationalInstruments.RFToolkits.Interop.Fx40", "NationalInstruments.RFToolkits.Interop")
 
     # This function helps keep a global reference to an open RFmx session on the server
     def GetGlobalInstrMX(self, resourceName, optionString):
