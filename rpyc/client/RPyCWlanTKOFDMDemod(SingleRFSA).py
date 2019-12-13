d400ef02cf76033f399f0f4caab412b713fcb492
Sean Moore <sean.moore@ni.com>
2019-12-13 07:57:23 -0600

    add generation

--- rpyc/client/RPyCWlanTKOFDMDemod(SingleRFSA).py
+++ rpyc/client/RPyCWlanTKOFDMDemod(SingleRFSA).py
@@ -15,17 +15,27 @@ ModInstInterop = conn.root.ModularInstrumentsInterop
 WlanTK = conn.root.WlanTK
 print("done")
 
-# # # # # User Parameters # # # # # #
+# # # # # Analysis Parameters # # # # # #
 print("Setting measurement parameters..", end = '')
 analyzerResourceName = "VST1_01"       # Resource name of the Analyzer on the Host
-standard = WlanTK.niWLANAConstants.Standard80211acMimoOfdm
-chBW = 20.00e6
+analyzerClockSource = "OnboardClock"
+refLevel = 0
+analyzerExternalAttenuation = 0
 ofdmNumOfAverages = 10
 ofdmMaximumSymbolsUsed = 16
-carrierFrequency = 5.180e9
-externalAttenuation = 0
 timeout = 10
-refLevel = 0
+
+# # # # # Generation Parameters # # # # #
+generatorResournceName = "VST1_01"
+generatorClockSource = "OnboardClock"
+powerLevel = -10
+generatorExternalAttenuation = 0
+mcsIndex = 0
+
+# # # # # Common Parameters # # # # #
+chBW = 20.00e6
+carrierFrequency = 5.180e9
+standard = WlanTK.niWLANAConstants.Standard80211acMimoOfdm
 print("done")
 
 # # # # # Measurement Configuration # # # # #
@@ -42,11 +52,37 @@ print("done")
 # # # # # Analyzer Configuration # # # # #
 print("Configuring analyzer..", end = '')
 rfsaSession = ModInstInterop.niRFSA(analyzerResourceName, True, False)
-rfsaSession.ConfigureRefClock("PXI_Clk", 10e6)
-rfsaSession.SetExternalGain(None, -(externalAttenuation))
+rfsaSession.ConfigureRefClock(analyzerClockSource, 10e6)
+rfsaSession.SetExternalGain(None, -(analyzerExternalAttenuation))
 rfsaSession.ConfigureReferenceLevel(None, refLevel)
 rfsaSession.ConfigureIQPowerEdgeRefTrigger("0", refLevel - 20.0, ModInstInterop.niRFSAConstants.RisingSlope, 0.0)
 wlana.RFSAConfigureFrequencySingleLO([rfsaSession.Handle], WlanTK.niWLANAConstants.LOSourceOnboard, System.Runtime.InteropServices.HandleRef(), carrierFrequency, False, False)
+print("done")
+
+# # # # # Waveform Configuration # # # # #
+print("Configuring waveform..", end = '')
+wlang = WlanTK.niWLANG(WlanTK.niWLANGConstants.CompatibilityVersion050000)
+wlang.SetStandard(None, standard)
+wlang.SetChannelBandwidth(None, chBW)
+wlang.SetMcsIndex(None, mcsIndex)
+print("done")
+
+# # # # # Generator Configuration # # # # #
+print("Configuring generator..", end = '')
+rfsgSession = ModInstInterop.niRFSG(generatorResournceName, True, False)
+rfsgSession.ConfigurePowerLevelType(ModInstInterop.niRFSGConstants.PeakPower)
+rfsgSession.ConfigureGenerationMode(ModInstInterop.niRFSGConstants.Script)
+rfsgSession.ConfigureRefClock(generatorClockSource, 10e6)
+rfsgSession.SetExternalGain(None, -(generatorExternalAttenuation))
+
+wlang.SetRFBlankingEnabled(None, False)
+wlang.RFSGConfigureFrequencySingleLO([rfsgSession.Handle], WlanTK.niWLANGConstants.LOSourceOnboard, System.Runtime.InteropServices.HandleRef(), carrierFrequency, False, False)
+waveformName = "wlangtkwfm"
+wlang.RFSGCreateAndDownloadMIMOWaveforms([rfsgSession.Handle], None, 1, waveformName)
+script = "script GenerateWlan\nrepeat forever\ngenerate " + waveformName + "\n end repeat\n end script"
+WlanTK.niWLANG.WLANG_RFSGConfigureScript(rfsgSession.Handle, None, script, powerLevel)
+wlang.Close()
+rfsgSession.Initiate()
 print("done")
 
 # # # # # Measure and Print Results # # # # #
@@ -58,3 +94,4 @@ print("RMS EVM Mean (dB): {0:.3f}".format(rmsEvm))
 
 # # # # # Clean Up # # # # #
 rfsaSession.Close()
+rfsgSession.close()
