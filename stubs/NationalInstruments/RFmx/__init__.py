053ca22771b230e6cf1006029f833a10c3bdc4ee
Sean Moore <sean.moore@ni.com>
2020-02-11 06:43:37 -0600

    initial stubs for rfmx modules

---
+++ stubs/NationalInstruments/RFmx/__init__.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# module NationalInstruments.RFmx calls itself RFmx
+# from NationalInstruments.RFmx.InstrMX.Fx40, Version=19.1.0.49152, Culture=neutral, PublicKeyToken=dc6ad606294fc298
+# by generator 1.145
+# no doc
+# no imports
+
+# no functions
+# no classes
+# variables with complex values
+
